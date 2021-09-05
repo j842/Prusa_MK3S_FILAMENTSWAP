e9ce42f4f30c55d4add6e814d8e9f1b30a3cc40c
j842 <j@jde.nz>
2021-09-05 14:03:26 +1200

    Initial commit

---
+++ materials/PETG-3DEA.lua
@@ -0,0 +1,15 @@
+name_en = "PETG - 3DEA"
+name_fr = "PETG"
+name_es = "PETG"
+
+filament_linear_adv_factor = 45
+
+-- 200-240, 75-90
+extruder_temp_degree_c_0 = 228
+bed_temp_degree_c = 82
+
+filament_priming_mm_0 = 0.8
+
+enable_fan = true
+fan_speed_percent = 50 -- from 20% to 50%
+fan_speed_percent_on_bridges = 100
---
+++ materials/PETG-Sunlu.lua
@@ -0,0 +1,15 @@
+name_en = "PETG - Sunlu"
+name_fr = "PETG"
+name_es = "PETG"
+
+filament_linear_adv_factor = 45
+
+-- depending on colour! {205 to 230, 215 to 230, 220 to 250, 240+-10}, 70-80 bed temp
+extruder_temp_degree_c_0 = 227
+bed_temp_degree_c = 75
+
+filament_priming_mm_0 = 0.8
+
+enable_fan = true
+fan_speed_percent = 50 -- from 20% to 50%
+fan_speed_percent_on_bridges = 100
