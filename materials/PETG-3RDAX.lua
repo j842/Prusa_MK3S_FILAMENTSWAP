f1be2b9c0debb0e6f4c2fe251a4cbb86523e0eff
j842 <j@jde.nz>
2021-09-08 16:14:16 +1200

    Add 3RDAX profile.

---
+++ materials/PETG-3RDAX.lua
@@ -0,0 +1,14 @@
+name_en = "PETG-3RDAX"
+name_fr = "PETG-3RDAX"
+name_es = "PETG-3RDAX"
+
+filament_linear_adv_factor = 45
+
+extruder_temp_degree_c_0 = 222
+bed_temp_degree_c = 80
+
+filament_priming_mm_0 = 0.8
+
+enable_fan = true
+fan_speed_percent = 50 -- from 20% to 50%
+fan_speed_percent_on_bridges = 100
